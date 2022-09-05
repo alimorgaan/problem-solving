a97f851001f6af31871768948445eaa881f2d310
Ali Morgan <alimorgaan@gmail.com>
2022-09-05 15:29:06 +0200

    upload

--- cses/Introductory-Problems/palindromeReorder.cpp
+++ cses/Introductory-Problems/palindromeReorder.cpp
@@ -1,72 +1,63 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
 
 using namespace std;
 int main()
 {
-    string myString;
-    cin >> myString;
-    char oddChar;
-    int oddFlag = 0;
-    int counter = 1;
-    int oddIndex;
-    sort(myString.begin(), myString.end());
+    string input;
+    int oddCount = 0;
+    int oddIndex = 26;
+    cin >> input;
 
-    for (int i = 0; i < myString.size(); i++)
+    unsigned int freq[26] = {0};
+
+    for (int i = 0; i < input.size(); i++)
     {
-        if (i == myString.size() - 1)
-        {
-            if (counter % 2)
-            {
-                // odd
-                oddFlag++;
-                oddIndex = i;
-                oddChar = myString[i]; 
-                break; 
-            }
-        }
-        if (myString[i] == myString[i + 1])
-        {
-            counter++;
-        }
-        else
-        {
+        unsigned int myIndex = input[i] - 'A';
+        freq[myIndex]++;
+    }
 
-            if (counter % 2)
-            {
-                // odd
-                oddFlag++;
-                oddChar = myString[i];
-                oddIndex = i;
-            }
-            counter = 1;
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] % 2)
+        {
+            // odd
+            oddCount++;
+            oddIndex = i;
         }
     }
-    if (oddFlag > 1)
+
+    if (oddCount > 1)
     {
         cout << "NO SOLUTION" << endl;
         return 0;
     }
-    else if (oddFlag == 1)
-    {
-        myString.erase(oddIndex, 1);
-    }
 
-    for (int i = 0; i < myString.size() / 2; i++)
+    string myString = "";
+
+    for (int i = 0; i < 26; i++)
     {
-        if (myString[i] != myString[myString.size() - 1 - i])
+        if (i != oddIndex)
         {
-            
-            myString.insert(myString.size() - i, string(1, myString[i]));
-            myString.erase(myString.begin() + i);
+            myString = myString + string(freq[i] / 2, 'A' + i);
         }
     }
-    if(oddFlag == 1){
-        myString.insert(myString.size() / 2, string(1, oddChar));
-    }
-    
 
-    cout << myString << endl;
+    string output;
+
+    if (oddCount == 1)
+    {
+        output = myString + string(freq[oddIndex], 'A' + oddIndex);
+        reverse(myString.begin(), myString.end());
+        output = output + myString;
+    }
+    else
+    {
+        output = myString;
+        reverse(myString.begin(), myString.end());
+        output = output + myString;
+    }
+
+    cout << output << endl;
     return 0;
 }
